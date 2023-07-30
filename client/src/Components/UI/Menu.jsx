f0887bb60cb64afa303daea98ef0dd2bb99ec9cf
PabloPicas0 <pawel.fe@gmail.com>
2023-07-30 17:30:13 +0200

    add to list form is separate component

--- client/src/Components/UI/Menu.jsx
+++ client/src/Components/UI/Menu.jsx
@@ -1,30 +1,12 @@
 import { Person, HomeOutlined, LogoutSharp, Settings, Add } from "@mui/icons-material";
-import {
-  IconButton,
-  Tooltip,
-  Zoom,
-  Grow,
-  Box,
-  Dialog,
-  DialogTitle,
-  TextField,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  Typography,
-  Rating,
-  Button,
-  DialogContent,
-  DialogActions,
-} from "@mui/material";
+import { IconButton, Tooltip, Zoom, Grow, Box } from "@mui/material";
 
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import url from "../Utils/api";
 import { useDispatch, useSelector } from "react-redux";
-import { handleClientList } from "../Redux/Slices/profileSclice";
+
 import { handleDialog, handleVisibility } from "../Redux/Slices/menuSlice";
+
+import AddToList from "./addToList";
 
 const menuStyles = {
   menu: {
@@ -46,68 +28,12 @@ const menuStyles = {
     flexDirection: "column",
     gap: "25px",
   },
-  dialogBody: {
-    width: "100%",
-  },
-  score: {
-    display: "flex",
-    flexDirection: { xs: "column", sm: "row" },
-    gap: "10px",
-    marginTop: "20px",
-  },
 };
 
 const Menu = () => {
-  const [dialogValues, setDialogValues] = useState({
-    title: "",
-    status: "Plan to watch",
-    watchedEp: 0,
-    allEp: 0,
-    score: 0,
-  });
-
-  const username = useSelector((state) => state.profile.profileFields.username);
   const isVisible = useSelector((state) => state.menu.isVisible);
-  const openDialog = useSelector((state) => state.menu.openDialog);
 
   const dispatch = useDispatch();
-
-  const handleClose = () => {
-    dispatch(handleDialog(false));
-    setDialogValues({
-      title: "",
-      status: "Plan to watch",
-      watchedEp: 0,
-      allEp: 0,
-      score: 0,
-    });
-  };
-
-  // TODO
-  // Form is subbmited with empty fields
-  const handleSubmit = async () => {
-    console.log(username);
-    try {
-      const request = await fetch(`${url}/api/list`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: `username=${username}&type=${"ova"}&title=${dialogValues.title}&status=${
-          dialogValues.status
-        }&currentEp=${dialogValues.watchedEp}&allEp=${dialogValues.allEp}&score=${dialogValues.score}`,
-      });
-
-      const response = await request.json();
-
-      console.log(response);
-
-      dispatch(handleClientList(response.list));
-      dispatch(handleDialog(false));
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   return (
     <Box sx={menuStyles.menu}>
@@ -164,110 +90,7 @@ const Menu = () => {
         </Box>
       </Box>
 
-      <Dialog
-        onClose={() => handleClose()}
-        open={openDialog}
-        PaperProps={{ style: menuStyles.dialogBody }}
-        disableScrollLock>
-        <DialogTitle textAlign={"center"}>Add to list</DialogTitle>
-
-        <DialogContent>
-          <TextField
-            label="Anime title"
-            required
-            margin="dense"
-            fullWidth
-            value={dialogValues.title}
-            onChange={(e) =>
-              setDialogValues((oldValues) => {
-                const newValues = { ...oldValues };
-                newValues.title = e.target.value;
-
-                return newValues;
-              })
-            }
-          />
-
-          <FormControl fullWidth margin="dense">
-            <InputLabel id="select-label">Status</InputLabel>
-
-            <Select
-              label="Status"
-              labelId="select-label"
-              value={dialogValues.status}
-              MenuProps={{
-                marginThreshold: 10,
-              }}
-              onChange={(e) =>
-                setDialogValues((oldValues) => {
-                  const newValues = { ...oldValues };
-                  newValues.status = e.target.value;
-
-                  return newValues;
-                })
-              }>
-              <MenuItem value={"Currently watching"}>Currently watching</MenuItem>
-              <MenuItem value={"Completed"}>Completed</MenuItem>
-              <MenuItem value={"Plan to watch"}>Plan to watch</MenuItem>
-            </Select>
-          </FormControl>
-
-          <TextField
-            type="number"
-            label="Episodes watched"
-            margin="dense"
-            fullWidth
-            inputProps={{ min: 0 }}
-            onChange={(e) =>
-              setDialogValues((oldValues) => {
-                const newValues = { ...oldValues };
-                newValues.watchedEp = e.target.value;
-
-                return newValues;
-              })
-            }
-          />
-
-          <TextField
-            type="number"
-            label="All episodes"
-            margin="dense"
-            fullWidth
-            inputProps={{ min: 0 }}
-            required
-            onChange={(e) =>
-              setDialogValues((oldValues) => {
-                const newValues = { ...oldValues };
-                newValues.allEp = e.target.value;
-
-                return newValues;
-              })
-            }
-          />
-
-          <Box sx={menuStyles.score}>
-            <Typography>Your score:</Typography>
-            <Rating
-              name="anime-rating"
-              value={dialogValues.score}
-              onChange={(e, newValue) => {
-                setDialogValues((oldValues) => {
-                  const newValues = { ...oldValues };
-                  newValues.score = newValue;
-
-                  return newValues;
-                });
-              }}
-            />
-          </Box>
-        </DialogContent>
-
-        <DialogActions>
-          <Button variant="contained" fullWidth type="submit" onClick={handleSubmit}>
-            Submit
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <AddToList />
     </Box>
   );
 };
---
+++ client/src/Components/UI/addToList.jsx
@@ -0,0 +1,195 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  Dialog,
+  DialogTitle,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Typography,
+  Rating,
+  Button,
+  DialogContent,
+  DialogActions,
+  Box,
+} from "@mui/material";
+
+import { handleDialog } from "../Redux/Slices/menuSlice";
+import { handleClientList } from "../Redux/Slices/profileSclice";
+
+import url from "../Utils/api";
+
+const addToListStyles = {
+  dialogBody: {
+    width: "100%",
+  },
+  score: {
+    display: "flex",
+    flexDirection: { xs: "column", sm: "row" },
+    gap: "10px",
+    marginTop: "20px",
+  },
+};
+
+const AddToList = () => {
+  const [dialogValues, setDialogValues] = useState({
+    title: "",
+    status: "Plan to watch",
+    watchedEp: 0,
+    allEp: 0,
+    score: 0,
+  });
+  const username = useSelector((state) => state.profile.profileFields.username);
+  const openDialog = useSelector((state) => state.menu.openDialog);
+
+  const dispatch = useDispatch();
+
+  const handleClose = () => {
+    dispatch(handleDialog(false));
+    setDialogValues({
+      title: "",
+      status: "Plan to watch",
+      watchedEp: 0,
+      allEp: 0,
+      score: 0,
+    });
+  };
+
+  // TODO
+  // Form is subbmited with empty fields
+  const handleSubmit = async () => {
+    console.log(username);
+    try {
+      const request = await fetch(`${url}/api/list`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `username=${username}&type=${"ova"}&title=${dialogValues.title}&status=${
+          dialogValues.status
+        }&currentEp=${dialogValues.watchedEp}&allEp=${dialogValues.allEp}&score=${dialogValues.score}`,
+      });
+
+      const response = await request.json();
+
+      console.log(response);
+
+      dispatch(handleClientList(response.list));
+      dispatch(handleDialog(false));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Dialog
+      onClose={() => handleClose()}
+      open={openDialog}
+      PaperProps={{ style: addToListStyles.dialogBody }}
+      disableScrollLock>
+      <DialogTitle textAlign={"center"}>Add to list</DialogTitle>
+
+      <DialogContent>
+        <TextField
+          label="Anime title"
+          required
+          margin="dense"
+          fullWidth
+          value={dialogValues.title}
+          onChange={(e) =>
+            setDialogValues((oldValues) => {
+              const newValues = { ...oldValues };
+              newValues.title = e.target.value;
+
+              return newValues;
+            })
+          }
+        />
+
+        <FormControl fullWidth margin="dense">
+          <InputLabel id="select-label">Status</InputLabel>
+
+          <Select
+            label="Status"
+            labelId="select-label"
+            value={dialogValues.status}
+            MenuProps={{
+              marginThreshold: 10,
+            }}
+            onChange={(e) =>
+              setDialogValues((oldValues) => {
+                const newValues = { ...oldValues };
+                newValues.status = e.target.value;
+
+                return newValues;
+              })
+            }>
+            <MenuItem value={"Currently watching"}>Currently watching</MenuItem>
+            <MenuItem value={"Completed"}>Completed</MenuItem>
+            <MenuItem value={"Plan to watch"}>Plan to watch</MenuItem>
+          </Select>
+        </FormControl>
+
+        <TextField
+          type="number"
+          label="Episodes watched"
+          margin="dense"
+          fullWidth
+          inputProps={{ min: 0 }}
+          onChange={(e) =>
+            setDialogValues((oldValues) => {
+              const newValues = { ...oldValues };
+              newValues.watchedEp = e.target.value;
+
+              return newValues;
+            })
+          }
+        />
+
+        <TextField
+          type="number"
+          label="All episodes"
+          margin="dense"
+          fullWidth
+          inputProps={{ min: 0 }}
+          required
+          onChange={(e) =>
+            setDialogValues((oldValues) => {
+              const newValues = { ...oldValues };
+              newValues.allEp = e.target.value;
+
+              return newValues;
+            })
+          }
+        />
+
+        <Box sx={addToListStyles.score}>
+          <Typography>Your score:</Typography>
+          <Rating
+            name="anime-rating"
+            value={dialogValues.score}
+            onChange={(e, newValue) => {
+              setDialogValues((oldValues) => {
+                const newValues = { ...oldValues };
+                newValues.score = newValue;
+
+                return newValues;
+              });
+            }}
+          />
+        </Box>
+      </DialogContent>
+
+      <DialogActions>
+        <Button variant="contained" fullWidth type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default AddToList;
