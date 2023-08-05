650987bfd311c43dd706db63ff0109256ce97505
PabloPicas0 <pawel.fe@gmail.com>
2023-08-05 15:28:53 +0200

    styled new setting route page

--- client/src/Components/Routes/Settings.jsx
+++ client/src/Components/Routes/Settings.jsx
@@ -11,6 +11,18 @@ const settingsStyles = {
     padding: "3rem 10px",
     minHeight: "100vh",
   },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "30px"
+  },
+  setting: { 
+    display: "flex", 
+    maxWidth: "340px",
+    justifyContent: "space-between",
+    alignItems: "center" ,
+    gap: "20px"
+  }
 };
 
 const Settings = () => {
@@ -18,13 +30,13 @@ const Settings = () => {
     <Box sx={settingsStyles.container}>
       <Menu />
 
-      <Box>
-        <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={settingsStyles.wrapper}>
+        <Box sx={settingsStyles.setting}>
           <Typography>Keep logined</Typography>
           <Switch />
         </Box>
 
-        <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Box sx={settingsStyles.setting}>
           <Typography>Profile main color</Typography>
           <FormControl size="small" sx={{ minWidth: "180px" }}>
             <InputLabel id="demo-simple-select-label">Color</InputLabel>
@@ -36,7 +48,7 @@ const Settings = () => {
           </FormControl>
         </Box>
 
-        <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Box sx={settingsStyles.setting}>
           <Typography>Profile font</Typography>
           <FormControl size="small" sx={{ minWidth: "180px" }}>
             <InputLabel id="demo-simple-select-label">Font</InputLabel>
@@ -48,11 +60,11 @@ const Settings = () => {
           </FormControl>
         </Box>
 
-        <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Box sx={settingsStyles.setting}>
           <Typography>Default List</Typography>
           <FormControl size="small" sx={{ minWidth: "180px" }}>
             <InputLabel id="demo-simple-select-label">All anime</InputLabel>
-            <Select labelId="demo-simple-select-label" id="demo-simple-select" label="Filter">
+            <Select labelId="demo-simple-select-label" id="demo-simple-select" label="All anime">
               <MenuItem value={10}>All anime</MenuItem>
               <MenuItem value={20}>Currently Watchin</MenuItem>
               <MenuItem value={30}>Completed</MenuItem>
