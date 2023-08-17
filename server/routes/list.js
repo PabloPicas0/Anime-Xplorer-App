c208f893df0ac0fb0c22fef1cdccce07333a323c
PabloPicas0 <pawel.fe@gmail.com>
2023-08-17 16:23:42 +0200

    added server validation preventing from adding same anime to the list

--- server/routes/list.js
+++ server/routes/list.js
@@ -4,13 +4,31 @@ const { check } = require("express-validator");
 const router = express.Router();
 
 const addToUserList = require("../controllers/addToList");
-const authUser = require("../middleware/authUser")
+const authUser = require("../middleware/authUser");
+
+const userModel = require("../models/User");
 
 router.post(
   "/",
   check("title", "Title is required").notEmpty(),
   check("allEp", "All episodes is required").not().equals("0").isNumeric(),
   authUser,
+  check("Authorization").custom(async (token, { req }) => {
+    const user = await userModel.findById(req.user.userId);
+
+    const { animeList } = user;
+    const { title } = req.body;
+
+    const titleExists = animeList.some((anime) => {
+      const { animeName } = anime;
+
+      return animeName === title;
+    });
+
+    if (titleExists) {
+      throw new Error("This anime is already in your list.");
+    }
+  }),
   addToUserList
 );
 
