932b64db718e238858e99a081af4a0f350f60eb8
PabloPicas0 <pawel.fe@gmail.com>
2023-08-24 15:05:53 +0200

    abstracted styles in editList component

--- client/src/Components/UI/EditList.jsx
+++ client/src/Components/UI/EditList.jsx
@@ -26,6 +26,31 @@ import { handleEditDialog } from "../Redux/Slices/menuSlice";
 // TODO
 // Find a way to render menu items dynamicly based on max episodes of card
 
+const editListStyles = {
+  dialogBody: {
+    style: {
+      width: "100%",
+    },
+  },
+  animeNameBox: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginTop: "8px",
+    marginBottom: "4px",
+  },
+  menuProps: {
+    marginThreshold: 10,
+  },
+  animeScoreBox: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginTop: "8px",
+  },
+  dialogActions: {
+    justifyContent: "center",
+  },
+};
+
 const EditList = (props) => {
   const isEditVisible = useSelector((state) => state.menu.openEditDialog);
 
@@ -39,12 +64,12 @@ const EditList = (props) => {
     <Dialog
       onClose={handleClose}
       open={isEditVisible}
-      PaperProps={{ style: { width: "100%" } }}
+      PaperProps={editListStyles.dialogBody}
       disableScrollLock>
       <DialogTitle textAlign={"center"}>Edit Anime</DialogTitle>
 
       <DialogContent>
-        <Box sx={{ display: "flex", justifyContent: "space-between", marginTop: "8px", marginBottom: "4px" }}>
+        <Box sx={editListStyles.animeNameBox}>
           <Typography>Anime name:</Typography>
           <Typography>Name</Typography>
         </Box>
@@ -52,13 +77,7 @@ const EditList = (props) => {
         <FormControl fullWidth margin="dense">
           <InputLabel id="select-label">Status</InputLabel>
 
-          <Select
-            label="Status"
-            labelId="select-label"
-            value={0}
-            MenuProps={{
-              marginThreshold: 10,
-            }}>
+          <Select label="Status" labelId="select-label" value={0} MenuProps={editListStyles.menuProps}>
             <MenuItem value={"Currently watching"}>Currently watching</MenuItem>
             <MenuItem value={"Completed"}>Completed</MenuItem>
             <MenuItem value={"Plan to watch"}>Plan to watch</MenuItem>
@@ -72,9 +91,7 @@ const EditList = (props) => {
             label="Episodes Watched"
             labelId="episodes-label"
             value={0}
-            MenuProps={{
-              marginThreshold: 10,
-            }}>
+            MenuProps={editListStyles.menuProps}>
             <MenuItem value={"Currently watching"}>Currently watching</MenuItem>
             <MenuItem value={"Completed"}>Completed</MenuItem>
             <MenuItem value={"Plan to watch"}>Plan to watch</MenuItem>
@@ -84,28 +101,24 @@ const EditList = (props) => {
         <FormControl fullWidth margin="dense">
           <InputLabel id="type-label">Anime Type</InputLabel>
 
-          <Select
-            label="Anime Type"
-            labelId="type-label"
-            value={0}
-            MenuProps={{
-              marginThreshold: 10,
-            }}>
+          <Select label="Anime Type" labelId="type-label" value={0} MenuProps={editListStyles.menuProps}>
             <MenuItem value={"TV"}>TV</MenuItem>
             <MenuItem value={"OVA"}>OVA</MenuItem>
             <MenuItem value={"MOVIE"}>MOVIE</MenuItem>
           </Select>
         </FormControl>
 
-        <Box sx={{ display: "flex", justifyContent: "space-between", marginTop: "8px" }}>
+        <Box sx={editListStyles.animeScoreBox}>
           <Typography>Edit score:</Typography>
           <Rating name="new-socre" value={5} />
         </Box>
       </DialogContent>
 
-      <DialogActions sx={{ justifyContent: "center" }}>
-          <Button variant="contained">Submit</Button>
-          <Button variant="contained" color="error">Delete</Button>
+      <DialogActions sx={editListStyles.dialogActions}>
+        <Button variant="contained">Submit</Button>
+        <Button variant="contained" color="error">
+          Delete
+        </Button>
       </DialogActions>
     </Dialog>
   );
