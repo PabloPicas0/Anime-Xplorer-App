a62f4d3322b78711719688034c5ca8bc765c94d4
PabloPicas0 <pawel.fe@gmail.com>
2023-08-29 18:22:47 +0200

    delete entry controller test logic

---
+++ server/controllers/deleteEntry.js
@@ -0,0 +1,18 @@
+const userModel = require("../models/User");
+
+const deleteEntry = async (req, res) => {
+  const { userId } = req.user;
+  const { title } = req.body;
+
+  try {
+    const user = await userModel.findByIdAndUpdate(
+      userId,
+      { $pull: { animeList: { animeName: title } } },
+      { new: true }
+    );
+
+    console.log(user);
+  } catch (error) {}
+};
+
+module.exports = deleteEntry;
