18e9802c58821ec4de057d12678756eb07aa2be4
PabloPicas0 <pawel.fe@gmail.com>
2023-08-29 16:20:53 +0200

    added early return if user didnt change antything

--- client/src/Components/UI/EditList.jsx
+++ client/src/Components/UI/EditList.jsx
@@ -91,6 +91,8 @@ const EditList = (props) => {
   };
 
   const handleEdit = async () => {
+    if (!stateChanged) return;
+
     try {
       const request = await fetch(`${url}/api/list/edit`, {
         method: "PUT",
