06340563c1ace6513145c2eb211c1d9d6f901f11
PabloPicas0 <pawel.fe@gmail.com>
2023-09-03 20:03:55 +0200

    working controller

--- server/controllers/deleteUser.js
+++ server/controllers/deleteUser.js
@@ -4,10 +4,15 @@ const deleteUser = async (req, res) => {
   const { userId } = req.user;
 
   try {
-    const user = await userModel.findByIdAndDelete(userId)
+    const user = await userModel.findByIdAndDelete(userId);
 
+    return res.status(200).json({
+      error: false,
+      status: [{ msg: "OK" }],
+      isAuthenticated: false,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
 
     return res.status(500).json({
       error: true,
