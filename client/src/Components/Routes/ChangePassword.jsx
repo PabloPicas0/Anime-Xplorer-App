f54d159716e3e36f94cbd7bfebe17e1a465aa2b4
PabloPicas0 <pawel.fe@gmail.com>
2023-09-04 23:03:54 +0200

    created form basic look

--- client/src/Components/Routes/ChangePassword.jsx
+++ client/src/Components/Routes/ChangePassword.jsx
@@ -1,12 +1,38 @@
 import { useParams } from "react-router-dom";
 
+import { Box, Button, TextField } from "@mui/material";
+
+const ChangePassowrdStyles = {
+  container: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "20px",
+  },
+  formText: {
+    width: { xs: "223px", sm: "486px" },
+  },
+};
+
 const ChangePassowrd = () => {
   const { userId } = useParams();
 
   return (
-    <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+    <Box sx={ChangePassowrdStyles.container}>
       <h1>User id is {userId}</h1>
-    </div>
+
+      <TextField label="New password" autoComplete="on" fullWidth sx={ChangePassowrdStyles.formText} />
+      <TextField
+        label="Confirm new passowrd"
+        autoComplete="on"
+        fullWidth
+        sx={ChangePassowrdStyles.formText}
+      />
+
+    <Button variant="contained">Submit</Button>
+    </Box>
   );
 };
 
