9f07388a55f493d9f33c1931c11838e1cc7c3c88
PabloPicas0 <pawel.fe@gmail.com>
2023-09-05 21:48:01 +0200

    added put route for password change

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { authLogin, loadUser } = require("../controllers/login");
+const { authLogin, loadUser, changePassword } = require("../controllers/login");
 const authUser = require("../middleware/authUser");
 
 const { check } = require("express-validator");
@@ -13,6 +13,23 @@ router.post(
   check("username", "Username is required").notEmpty(),
   check("password", "Please check your password").exists(),
   authLogin
+);
+
+router.put(
+  "/",
+  check("password")
+    .notEmpty()
+    .withMessage("Please enter new password.")
+    .isLength({ min: 6 })
+    .withMessage("Password is too short.")
+    .custom(async (password, { req }) => {
+      if (password !== req.body.password2) {
+        throw new Error("Passwords don't match");
+      } else {
+        return password;
+      }
+    }),
+  changePassword
 );
 
 module.exports = router;
