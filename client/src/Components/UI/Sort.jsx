7fab06961bc2cbcda0c2ab5b5a3a17c71d6a42f8
PabloPicas0 <pawel.fe@gmail.com>
2023-09-14 18:07:11 +0200

    feat: create separate sort component

---
+++ client/src/Components/UI/Sort.jsx
@@ -0,0 +1,90 @@
+import { useDispatch, useSelector } from "react-redux";
+import { handleSort } from "../Redux/Slices/menuSlice";
+import { Button, ListItemIcon, ListItemText, Menu, MenuItem } from "@mui/material";
+import { North, SortSharp, South } from "@mui/icons-material";
+import { useRef } from "react";
+
+const sortStyles = {
+  colors: {
+    purple: "rgb(143 68 217)",
+    washedBlack: "rgba(0, 0, 0, 0.54)",
+    black: "rgb(0, 0, 0)",
+    hoverPurple: "rgb(243, 234, 251)",
+  },
+  menu: {
+    paper: {
+      style: {
+        borderRadius: "15px",
+      },
+    },
+    root: {
+      style: {
+        position: "absolute",
+        zIndex: 1099,
+      },
+    },
+  },
+  sortMenuList: {
+    style: {
+      display: "flex",
+      flexDirection: "column",
+      gap: "10px",
+    },
+  },
+  menuItemHover: {
+    "&:hover": {
+      color: "rgb(143, 68, 217)",
+      backgroundColor: "rgb(243, 234, 251)",
+    },
+  },
+};
+
+const Sort = (props) => {
+  const { setSortOrder } = props;
+
+  const sort = useSelector((state) => state.menu.sort);
+  const sortEl = useRef(null);
+
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <Button
+        ref={sortEl}
+        startIcon={
+          <SortSharp
+            sx={sort ? { color: sortStyles.colors.purple } : { color: sortStyles.colors.washedBlack }}
+          />
+        }
+        sx={sort ? { color: sortStyles.colors.purple } : { color: sortStyles.colors.washedBlack }}
+        onClick={(e) => {
+          dispatch(handleSort(true));
+        }}>
+        Sort
+      </Button>
+
+      <Menu
+        anchorEl={sortEl.current}
+        open={sort}
+        slotProps={sortStyles.menu}
+        MenuListProps={sortStyles.sortMenuList}
+        onClose={() => dispatch(handleSort(false))}>
+        <MenuItem onClick={() => setSortOrder("asc")} sx={sortStyles.menuItemHover}>
+          <ListItemIcon sx={{ color: "inherit" }}>
+            <North />
+          </ListItemIcon>
+          <ListItemText sx={sortStyles.menuText}>Sort Ascending</ListItemText>
+        </MenuItem>
+
+        <MenuItem onClick={() => setSortOrder("desc")} sx={sortStyles.menuItemHover}>
+          <ListItemIcon sx={{ color: "inherit" }}>
+            <South />
+          </ListItemIcon>
+          <ListItemText sx={sortStyles.menu}>Sort Descending</ListItemText>
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default Sort;
