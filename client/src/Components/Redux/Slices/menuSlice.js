35b2824f278a976dc7bfe6c90920774640e0be42
PabloPicas0 <pawel.fe@gmail.com>
2023-09-15 15:17:46 +0200

    chore: handle additional state

--- client/src/Components/Redux/Slices/menuSlice.js
+++ client/src/Components/Redux/Slices/menuSlice.js
@@ -6,6 +6,11 @@ export const menuSlice = createSlice({
     isVisible: false,
     openDialog: false,
     sort: false,
+    filter: false,
+    showSearch: false,
+    showType: false,
+    showScore: false,
+    showDate: false,
   },
   reducers: {
     handleVisibility: (state, action) => {
@@ -15,10 +20,14 @@ export const menuSlice = createSlice({
       state.openDialog = action.payload;
     },
     handleSort: (state, action) => {
-      state.sort = action.payload
-    }
+      state.sort = action.payload;
+    },
+    handleFilter: (state, action) => {
+      const { type, isActive } = action.payload;
+      state[type] = isActive;
+    },
   },
 });
 
-export const { handleVisibility, handleDialog, handleSort } = menuSlice.actions;
+export const { handleVisibility, handleDialog, handleSort, handleFilter } = menuSlice.actions;
 export default menuSlice.reducer;
