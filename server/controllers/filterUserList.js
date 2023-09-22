f22e1a6637938228e06690138b26da4efd684e60
PabloPicas0 <pawel.fe@gmail.com>
2023-09-22 13:40:33 +0200

    refactor: add all filter controllers

--- server/controllers/filterUserList.js
+++ server/controllers/filterUserList.js
@@ -1,27 +1,37 @@
 const userModel = require("../models/User");
 
-const checkScore = async (req, res, next) => {
-  const minScore = Number(req.body.minScore);
-  const maxScore = Number(req.body.maxScore);
-  const { userId } = req.user;
+const findScore = async (req, res, next) => {
+  try {
+    const minScore = Number(req.body.minScore);
+    const maxScore = Number(req.body.maxScore);
+    const { userId } = req.user;
 
-  const user = await userModel.findById(userId);
-  const { animeList } = user;
+    const user = await userModel.findById(userId);
+    const { animeList } = user;
 
-  req.list = [...animeList];
+    req.list = [...animeList];
 
-  if (minScore === 0 && maxScore === 0) next();
+    if (minScore === 0 && maxScore === 0) next();
 
-  const newList = animeList.filter((anime) => minScore <= anime.score && maxScore >= anime.score);
+    const newList = animeList.filter((anime) => minScore <= anime.score && maxScore >= anime.score);
 
-  req.list = newList;
+    req.list = newList;
 
-  next();
+    next();
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      error: true,
+      status: [{ msg: "Internal server error. Please try again later." }],
+    });
+  }
 };
 
-const checkDate = (req, res, next) => {
+const findDate = (req, res, next) => {
   const startDate = Number(req.body.startWatching);
   const endDate = Number(req.body.endWatching);
+  const animeList = req.list;
 
   if (startDate === 0 && endDate === 0) next();
 
@@ -32,17 +42,59 @@ const checkDate = (req, res, next) => {
     });
   }
 
-  console.log(req.list);
-};
-// TODO: Add error handling //
-const filterUserList = async (req, res) => {
-  try {
-    const { animeType, animeName,  } = req.body;
-    const { userId } = req.user;
+  const newList = animeList.filter((anime) => {
+    if (startDate !== 0 && endDate === 0) {
+      return startDate <= anime.startWatching;
+    }
+    if (startDate === 0 && endDate !== 0) {
+      return endDate >= anime.endWatching;
+    }
+    if (startDate !== 0 && endDate !== 0) {
+      return startDate <= anime.startWatching && endDate >= anime.endWatching;
+    }
+  });
 
-  } catch (error) {
-    console.log(error);
-  }
+  req.list = newList;
+
+  next();
 };
 
-module.exports = { filterUserList, checkScore, checkDate };
+const findType = (req, res, next) => {
+  const { animeType } = req.body;
+  const animeList = req.list;
+
+  if (animeType === "") next();
+
+  const newList = animeList.filter((anime) => anime.animeType === animeType);
+
+  req.list = newList;
+
+  next();
+};
+
+const findTitle = (req, res, next) => {
+  const { animeName } = req.body;
+  const animeList = req.list;
+
+  if (animeName === "") next();
+
+  const newList = animeList.filter((anime) =>
+    anime.animeName.toLowerCase().includes(animeName.toLowerCase())
+  );
+
+  req.list = newList;
+
+  next();
+};
+
+const sendToUser = (req, res) => {
+  const filteredList = req.list;
+
+  return res.status(200).json({
+    error: false,
+    status: [{ msg: "" }],
+    list: filteredList,
+  });
+};
+
+module.exports = { sendToUser, findScore, findDate, findType, findTitle };
