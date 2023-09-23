ae17712f1a8e2413fb83d40d44ae3812a9522114
PabloPicas0 <pawel.fe@gmail.com>
2023-09-23 19:18:14 +0200

    refactor: moved global theme to Theme component

--- client/src/Components/Theme/Theme.jsx
+++ client/src/Components/Theme/Theme.jsx
@@ -1,20 +1,36 @@
 import { ThemeProvider, createTheme } from "@mui/material";
 
-const theme = createTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 900,
-      lg: 1100,
-      xl: 1536,
-    },
-  },
-  typography: {
-    fontFamily: ["Inter", "system-ui", "Avenir", "Helvetica", "Arial", "sans-serif"].join(),
-  },
-});
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
 
 export const Theme = ({ children }) => {
+  const options = useSelector((state) => state.profile.profileFields.options[0]);
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        breakpoints: {
+          values: {
+            xs: 0,
+            sm: 600,
+            md: 900,
+            lg: 1100,
+            xl: 1536,
+          },
+        },
+        typography: {
+          fontFamily: [
+            `${options ? options.font : "Inter"}`,
+            "system-ui",
+            "Avenir",
+            "Helvetica",
+            "Arial",
+            "sans-serif",
+          ].join(),
+        },
+      }),
+    [options]
+  );
+
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
