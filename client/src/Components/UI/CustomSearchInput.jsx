eb3011e71949efed2a0ca3007b1809d6c80c8444
PabloPicas0 <pawel.fe@gmail.com>
2023-09-26 19:02:28 +0200

    feat: connect search bar with server

--- client/src/Components/UI/CustomSearchInput.jsx
+++ client/src/Components/UI/CustomSearchInput.jsx
@@ -1,8 +1,19 @@
 import { SearchRounded } from "@mui/icons-material";
-import { Collapse, Divider, IconButton, InputBase, Paper, Tooltip, Zoom } from "@mui/material";
+import {
+  Collapse,
+  Divider,
+  IconButton,
+  InputBase,
+  Menu,
+  MenuItem,
+  Paper,
+  Tooltip,
+  Zoom,
+} from "@mui/material";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import useSearch from "../Utils/useSearch";
 
 const customSearchInputStyles = {
   paper: {
@@ -31,37 +42,47 @@ const customSearchInputStyles = {
 
 const CustomSearchInput = () => {
   const [isSearchVisible, setIsSearchVisible] = useState(false);
+  const [userName, setUserName] = useState("");
 
   const darkMode = useSelector((state) => state.profile.profileFields.options[0].darkMode);
   const mainColor = darkMode ? customSearchInputStyles.colors.dark : customSearchInputStyles.colors.gray;
 
+  const users = useSearch(userName);
+  const customSearch = useRef(null);
+  console.log(users);
+
   return (
-    <Paper
-      component={"form"}
-      elevation={isSearchVisible ? 3 : 0}
-      sx={{
-        ...customSearchInputStyles.paper,
-        backgroundColor: mainColor,
-      }}>
-      <Collapse in={isSearchVisible} orientation="horizontal">
-        <InputBase
-          type="search"
-          placeholder="Find user"
-          inputProps={customSearchInputStyles.input.inputProps}
-          sx={customSearchInputStyles.input.inputStyles}
-        />
-      </Collapse>
+    <>
+      <Paper
+        ref={customSearch}
+        component={"form"}
+        elevation={isSearchVisible ? 3 : 0}
+        sx={{
+          ...customSearchInputStyles.paper,
+          backgroundColor: mainColor,
+        }}>
+        <Collapse in={isSearchVisible} orientation="horizontal">
+          <InputBase
+            type="search"
+            placeholder="Find user"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            inputProps={customSearchInputStyles.input.inputProps}
+            sx={customSearchInputStyles.input.inputStyles}
+          />
+        </Collapse>
 
-      <Collapse in={isSearchVisible} orientation="horizontal">
-        <Divider orientation="vertical" sx={customSearchInputStyles.divider} />
-      </Collapse>
+        <Collapse in={isSearchVisible} orientation="horizontal">
+          <Divider orientation="vertical" sx={customSearchInputStyles.divider} />
+        </Collapse>
 
-      <Tooltip TransitionComponent={Zoom} title="Search" arrow>
-        <IconButton size="large" onClick={() => setIsSearchVisible((prev) => !prev)}>
-          <SearchRounded />
-        </IconButton>
-      </Tooltip>
-    </Paper>
+        <Tooltip TransitionComponent={Zoom} title="Search" arrow>
+          <IconButton size="large" onClick={() => setIsSearchVisible((prev) => !prev)}>
+            <SearchRounded />
+          </IconButton>
+        </Tooltip>
+      </Paper>
+    </>
   );
 };
 
