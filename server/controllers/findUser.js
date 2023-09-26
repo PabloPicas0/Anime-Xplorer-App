bac75c6f8761feeb4a111d79f2ade42bc71f81a5
PabloPicas0 <pawel.fe@gmail.com>
2023-09-26 19:04:23 +0200

    feat: create controller responsible for finding users

---
+++ server/controllers/findUser.js
@@ -0,0 +1,20 @@
+const userModel = require("../models/User");
+
+const findUser = async (req, res) => {
+  const { user } = req.body;
+
+  try {
+    const searchUser = await userModel.findOne({ username: user });
+    console.log(user, searchUser);
+    return res.json({ users: searchUser, error: false, status: [{ msg: "" }] });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      error: true,
+      status: [{ msg: "Internal server error. Please try again later" }],
+    });
+  }
+};
+
+module.exports = findUser;
