01c1a9501b31a05a59897053fb89c54d67e5d220
PabloPicas0 <pawel.fe@gmail.com>
2023-10-09 23:28:45 +0200

    feat: add metadata for tracking watching to schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
+const months = {};
+
+for (let i = 1; i <= 12; i++) {
+  months[
+    new Date(`1995-${i < 10 ? "0" + i : i}-17T00:00:00`).toLocaleDateString("en-GB", { month: "short" })
+  ] = 0;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -59,6 +67,20 @@ const userSchema = new mongoose.Schema({
       score: Number,
     },
   ],
+  metadata: {
+    type: {
+      monthWatchedEpisodes: { type: Object },
+      monthCompletedEpisodes: {type: Object}
+    },
+    default: {
+      monthWatchedEpisodes: {
+        [new Date().toLocaleDateString("en-GB", { year: "numeric" })]: { ...months },
+      },
+      monthCompletedEpisodes: {
+        [new Date().toLocaleDateString("en-GB", { year: "numeric" })]: { ...months },
+      },
+    },
+  },
 });
 
 const userModel = mongoose.model("userModel", userSchema);
