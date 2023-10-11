51d900b802fe647267bf65d5b108de669ef7fc8d
PabloPicas0 <pawel.fe@gmail.com>
2023-10-11 22:01:40 +0200

    feat: add statistics reducer to store

--- client/src/Components/Redux/Store/Store.js
+++ client/src/Components/Redux/Store/Store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import profileReducer from "../Slices/profileSclice";
 import menuReducer from "../Slices/menuSlice";
 import statusReducer from "../Slices/statusSlice";
+import statisticsReducer from "../Slices/statisticsSlice";
 
 export default configureStore({
   reducer: {
     profile: profileReducer,
     menu: menuReducer,
     status: statusReducer,
+    statistics: statisticsReducer,
   },
 });
