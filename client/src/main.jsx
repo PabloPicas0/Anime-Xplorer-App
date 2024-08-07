45715d286b6a0ac1c5fba2225dbfde9aea5e4291
PabloPicas0 <pawel.fe@gmail.com>
2024-08-07 15:24:38 +0200

    refactor: delete unused dependency

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { createBrowserRouter, createHashRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
 import "./index.css";
